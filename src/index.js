a0b17f2e5053e367f85b6df3cea949ca0a0bc013
Rahul Suresh <rsuresh4@illinois.edu>
2023-09-17 08:25:26 -0500

    router changes for gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Root from "./components/Root/Root";
 import AppTheme from "./extras/theme";
 import "./index.css";
@@ -9,28 +9,18 @@ import Conference from "./pages/Conference/Conference";
 import Home from "./pages/Home/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// const router = createBrowserRouter([
-//     {
-//         path: "/",
-//         element: <Home />,
-//     },
-//     {
-//         path: "/conference",
-//         element: <Conference />,
-//     },
-// ]);
 
 root.render(
     <React.StrictMode>
         <ChakraProvider theme={AppTheme}>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route element={<Root />}>
                         <Route path="/" element={<Home />} />
                         <Route path="/conference" element={<Conference />} />
                     </Route>
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </ChakraProvider>
     </React.StrictMode>
 );
